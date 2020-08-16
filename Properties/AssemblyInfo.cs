b5a9c0a75da2dd84d3ff557ad7559bef2117a6ab
nathan <nathan@nbcaldwell.com>
2020-08-16 18:02:02 -0400

    Add transition function
    Also cleanup the project name change
    Resolves #5

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Yknytt")]
+[assembly: AssemblyTitle("YKnytt")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
--- knytt/GKnyttGame.cs
+++ knytt/GKnyttGame.cs
@@ -3,37 +3,39 @@ using YKnyttLib;
 
 public class GKnyttGame : Node2D
 {
-    // TODO: This is per-player stuff, and should eventually be abstracted
-    public GKnyttArea CurrentArea { get; private set; }
-    public GKnyttWorld World { get; private set; }
-    public Camera2D Camera { get; private set; }
+	// TODO: This is per-player stuff, and should eventually be abstracted
+	public GKnyttArea CurrentArea { get; private set; }
+	public GKnyttWorld World { get; private set; }
+	public Camera2D Camera { get; private set; }
 
-    public override void _Ready()
-    {
-        this.Camera = GetNode("Camera2D") as Camera2D;
-        this.World = GetNode("GKnyttWorld") as GKnyttWorld;
+	public override void _Ready()
+	{
+		this.Camera = GetNode("Camera2D") as Camera2D;
+		this.World = GetNode("GKnyttWorld") as GKnyttWorld;
 
-        this.loadDemo();
-    }
+		this.loadDemo();
+	}
 
-    private void loadDemo()
-    {
-        var wd = new Directory();
-        var e = wd.Open("./worlds/Nifflas - The Machine");
-        this.World.loadWorld(wd);
+	private void loadDemo()
+	{
+		var wd = new Directory();
+		var e = wd.Open("./worlds/Nifflas - The Machine");
+		this.World.loadWorld(wd);
 
-        this.changeArea(new KnyttPoint(1001, 1000));
-    }
+		this.changeArea(new KnyttPoint(1001, 1000));
+	}
 
-    public void changeArea(KnyttPoint new_area)
-    {
-        // This should inform the world that it needs an area loaded
-        this.CurrentArea = this.World.instantiateArea(new_area);
+	// Changes the current area
+	public void changeArea(KnyttPoint new_area)
+	{
+		// This should inform the world that it needs an area loaded
+		this.CurrentArea = this.World.instantiateArea(new_area);
+		this.beginTransitionEffects();
+	}
 
-        this.Camera.GlobalPosition = this.CurrentArea.GlobalCenter;
-
-        GD.Print(this.CurrentArea.GlobalPosition);
-        GD.Print(this.CurrentArea.GlobalCenter);
-        GD.Print(this.Camera.GlobalPosition);
-    }
+	// Handles transition effects
+	private void beginTransitionEffects()
+	{
+		this.Camera.GlobalPosition = this.CurrentArea.GlobalCenter;
+	}
 }
