3d98520558e8f4aef3597bf860c4660f80cde8cf
Nathaniel Caldwell <nbcwell@gmail.com>
2020-08-19 00:25:10 -0400

    Fix various audio channel corner cases

--- knytt/KnyttAudioChannel.cs
+++ knytt/KnyttAudioChannel.cs
@@ -11,16 +11,17 @@ public class KnyttAudioChannel : AudioStreamPlayer
     public float fadeTime = 5f;
 
     // Queued track
+    bool track_queued = false;
     private int q_track;
     private AudioStream q_stream;
 
     public void setTrack(int num)
     {
         // If it's the same track already playing nothing happens
-        if (num == this.TrackNumber) 
+        if (num == this.TrackNumber)
         {
             // If the song is fading out, reverse the fading
-            //resetAnimation();
+            this.clearQ();
             ((AnimationPlayer)GetNode("AnimationPlayer")).PlaybackSpeed = -(1f / this.fadeTime);
             return; 
         }
@@ -34,12 +35,17 @@ public class KnyttAudioChannel : AudioStreamPlayer
     private void changeTrack(int track, AudioStream stream)
     {
         // Queue gets changed no matter what
-        this.q_track = track;
-        this.q_stream = stream;
+        this.setQ(track, stream);
 
         var player = (AnimationPlayer)GetNode("AnimationPlayer");
-        // If already fading, simply change the queue
-        if (player.IsPlaying()) { return; }
+        //GD.Print(player.IsPlaying(), " ", this.Playing);
+        // If already fading out, simply change the queue
+        if (player.IsPlaying()) 
+        {
+            // Ensure that it's fading out, not back in
+            player.PlaybackSpeed = 1f / this.fadeTime; 
+            return; 
+        }
         // Else if this is actively playing a track, start a fade
         else if (this.Playing)
         {
@@ -52,8 +58,22 @@ public class KnyttAudioChannel : AudioStreamPlayer
         this.playQueued();
     }
 
+    private void clearQ()
+    {
+        this.track_queued = false;
+    }
+
+    private void setQ(int track, AudioStream stream)
+    {
+        this.track_queued = true;
+        this.q_track = track;
+        this.q_stream = stream;
+    }
+
     private void playQueued()
     {
+        if (!track_queued) { return; } // If not track queued, get out of here
+        this.track_queued = false;
         this.resetAnimation();
         this.Stop();
         this.TrackNumber = this.q_track;
@@ -70,5 +90,10 @@ public class KnyttAudioChannel : AudioStreamPlayer
         var player = (AnimationPlayer)GetNode("AnimationPlayer");
         player.Stop();
         this.VolumeDb = 0.0f;
+    }
+
+    public void _on_finished()
+    {
+        this.playQueued();
     }
 }
