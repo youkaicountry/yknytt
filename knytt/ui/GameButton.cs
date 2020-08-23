e869c632cb364cbc44c9aa1794ca5dbe02321ba4
nathan <nathan@nbcaldwell.com>
2020-08-23 19:02:29 -0400

    Fix GameButton fields

--- knytt/ui/GameButton.cs
+++ knytt/ui/GameButton.cs
@@ -8,10 +8,8 @@ public class GameButton : Button
     public void initialize(Texture icon, KnyttWorld<string> world)
     {
         this.World = world;
-        var main_container = GetNode("MainContainer");
-        ((TextureRect)(main_container.GetNode("IconTexture"))).Texture = icon;
-        var text_container = main_container.GetNode("TextContainer");
-        ((Label)(text_container.GetNode("NameLabel"))).Text = string.Format("{0} {1}", world.Info.Name, world.Info.Author);
-        ((Label)(text_container.GetNode("NameLabel"))).Text = world.Info.Description;
+        GetNode<TextureRect>("MainContainer/IconTexture").Texture = icon;
+        GetNode<Label>("MainContainer/TextContainer/NameLabel").Text = string.Format("{0} {1}", world.Info.Name, world.Info.Author);
+        GetNode<Label>("MainContainer/TextContainer/DescriptionLabel").Text = world.Info.Description;
     }
 }
