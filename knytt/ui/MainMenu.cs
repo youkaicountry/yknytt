ad5608360870d7d12245bd64ecac04012e4657d5
nathan <nathan@nbcaldwell.com>
2020-08-23 18:47:12 -0400

    Add quit button implementation
    Resolves #29

--- knytt/ui/MainMenu.cs
+++ knytt/ui/MainMenu.cs
@@ -10,8 +10,16 @@ public class MainMenu : Node2D
 
     public void _on_PlayButton_pressed()
     {
-        ((AudioStreamPlayer)GetNode("MenuClickPlayer")).Play();
+        GetNode<AudioStreamPlayer>("MenuClickPlayer").Play();
         var level_node = this.level_select_scene.Instance() as LevelSelection;
         this.AddChild(level_node);
+    }
+
+    public async void _on_QuitButton_pressed()
+    {
+        var player = GetNode<AudioStreamPlayer>("MenuClickPlayer");
+        player.Play();
+        await ToSignal(player, "finished");
+        GetTree().Quit();
     }
 }
