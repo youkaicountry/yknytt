76b4054c46402fe213c9139fdea3621d902c0729
nathan <nathan@nbcaldwell.com>
2020-08-23 20:28:13 -0400

    Add basic info screen
    Resolves #23

--- knytt/ui/GameButton.cs
+++ knytt/ui/GameButton.cs
@@ -5,11 +5,19 @@ public class GameButton : Button
 {
     public KnyttWorld<string> World { get; private set; }
 
+    [Signal]
+    public delegate void GamePressed();
+
     public void initialize(Texture icon, KnyttWorld<string> world)
     {
         this.World = world;
         GetNode<TextureRect>("MainContainer/IconTexture").Texture = icon;
         GetNode<Label>("MainContainer/TextContainer/NameLabel").Text = string.Format("{0} {1}", world.Info.Name, world.Info.Author);
         GetNode<Label>("MainContainer/TextContainer/DescriptionLabel").Text = world.Info.Description;
+    }
+
+    public void _on_GameButton_pressed()
+    {
+        EmitSignal(nameof(GamePressed), this);
     }
 }
