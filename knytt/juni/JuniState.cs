734f0cfe1b3d5ef46a99b953a86c37bb455990c5
nathan <nathan@nbcaldwell.com>
2020-08-26 05:16:14 -0400

    Finish (very) basic Juni implementation
    Resolves #14

--- knytt/juni/JuniState.cs
+++ knytt/juni/JuniState.cs
@@ -1,6 +1,3 @@
-using Godot;
-using System;
-
 public class IdleState : JuniState
 {
     public IdleState(Juni juni) : base(juni) { }
@@ -13,6 +10,12 @@ public class IdleState : JuniState
     public override void PreProcess(float delta)
     {
         if (juni.MoveDirection != 0) { juni.transitionState(new WalkRunState(juni)); } 
+    }
+
+    public override void PostProcess(float delta) 
+    {
+        if (juni.DidJump) { juni.transitionState(new JumpState(juni)); }
+        else if ( !juni.Grounded ) { juni.transitionState(new FallState(juni)); } // Ground falls out from under Juni
     }
 }
 
@@ -30,13 +33,75 @@ public class WalkRunState : JuniState
     public override void PreProcess(float delta)
     {
         var dir = juni.MoveDirection;
-        juni.velocity.x = juni.speed * dir;
+        juni.velocity.x = juni.speed * dir; // TODO: Get speed from a property
         if (dir == 0) { juni.transitionState(new IdleState(juni)); }
+    }
+
+    public override void PostProcess(float delta) 
+    { 
+        if (juni.DidJump) { juni.transitionState(new JumpState(juni)); }
+        else if ( !juni.Grounded ) { juni.transitionState(new FallState(juni)); } // Juni Walks off Ledge / ground falls out from under
     }
 
     public override void onExit()
     {
         juni.GetNode<RawAudioPlayer2D>("Audio/WalkPlayer2D").Stop();
+    }
+}
+
+public class JumpState : JuniState
+{
+    public JumpState(Juni juni) : base(juni) { }
+
+    public override void onEnter()
+    {
+        juni.Anim.Play("Jump");
+        juni.GetNode<RawAudioPlayer2D>("Audio/JumpPlayer2D").Play();
+        juni.velocity.y = juni.jump_speed;
+    }
+
+    public override void PreProcess(float delta)
+    {
+        var dir = juni.MoveDirection;
+        juni.velocity.x = juni.speed * dir; // TODO: Get speed from a property
+    }
+
+    public override void PostProcess(float delta)
+    { 
+        //if (juni) // Check if falling
+        if (juni.Grounded) 
+        { 
+            juni.transitionState(new IdleState(juni)); 
+            juni.GetNode<RawAudioPlayer2D>("Audio/LandPlayer2D").Play();
+        }
+        // If 
+        else if (juni.velocity.y >= 0) { juni.transitionState(new FallState(juni)); }
+    }
+}
+
+public class FallState : JuniState
+{
+    public FallState(Juni juni) : base(juni) { }
+
+    public override void onEnter()
+    {
+        juni.Anim.Play("StartFall");
+    }
+
+    public override void PreProcess(float delta)
+    {
+        var dir = juni.MoveDirection;
+        juni.velocity.x = juni.speed * dir; // TODO: Get speed from a property
+    }
+
+    public override void PostProcess(float delta)
+    { 
+        //if (juni) // Check if falling
+        if (juni.Grounded) 
+        { 
+            juni.transitionState(new IdleState(juni)); 
+            juni.GetNode<RawAudioPlayer2D>("Audio/LandPlayer2D").Play();
+        }
     }
 }
 
