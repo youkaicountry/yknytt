95857f7e348a0c0ccfb7160a4d2890c22c483d78
Nathaniel Caldwell <nbcwell@gmail.com>
2020-09-15 23:34:52 -0400

    Add UI input configuration

---
+++ knytt/ui/InputScreen.cs
@@ -0,0 +1,68 @@
+using Godot;
+
+public class InputScreen : Control
+{
+    InputOption collecting;
+    int cnum;
+
+    public override void _Ready()
+    {
+        var settings = GetNode<Node>("SettingsContainer");
+        foreach (var child in settings.GetChildren())
+        {
+            var io = child as InputOption;
+            io.Screen = this;
+            io.Connect("GetActionInput", this, "_onPress");
+        }
+    }
+
+    public void _on_GDKnyttButton_pressed()
+    {
+        GDKnyttKeys.applyAllSettings();
+        GDKnyttKeys.saveSettings();
+        GetNode<AudioStreamPlayer>("../../MenuClickPlayer")?.Play();
+        QueueFree();
+    }
+
+    public void _onPress(InputOption io, int num)
+    {
+        if (collecting != null || GetNode<Timer>("BounceTimer").TimeLeft > 0) { return; }
+        GetNode<AudioStreamPlayer>("../../MenuClickPlayer")?.Play();
+        startCollecting(io, num);
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (collecting == null) { return; }
+        if (GDKnyttKeys.setAction(collecting.Action + cnum, @event))
+        {
+            finishCollecting();
+        }
+    }
+
+    private void startCollecting(InputOption io, int num)
+    {
+        collecting = io;
+        cnum = num;
+        io.setCollecting(num);
+        GetNode<Control>("KeyPrompt").Visible = true;
+        GetNode<Button>("BackButton").Disabled = true;
+        GetNode<Button>("BackButton").FocusMode = FocusModeEnum.None;
+    }
+
+    private void finishCollecting()
+    {
+        GetNode<Timer>("BounceTimer").Start();
+        collecting.refreshButtons();
+        collecting = null;
+        GetNode<Control>("KeyPrompt").Visible = false;
+        GetNode<Button>("BackButton").Disabled = false;
+        GetNode<Button>("BackButton").FocusMode = FocusModeEnum.All;
+    }
+
+    public void _on_CancelButton_pressed()
+    {
+        GetNode<AudioStreamPlayer>("../../MenuClickPlayer")?.Play();
+        finishCollecting();
+    }
+}
