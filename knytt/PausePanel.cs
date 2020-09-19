554821db92144b762502fb6d8f6581da5a9191d2
nathan <nathan@nbcaldwell.com>
2020-09-18 20:13:50 -0400

    Add pause menu

---
+++ knytt/PausePanel.cs
@@ -0,0 +1,58 @@
+using Godot;
+
+public class PausePanel : Panel
+{
+    PackedScene settings_scene;
+
+    public override void _Ready()
+    {
+        settings_scene = ResourceLoader.Load<PackedScene>("res://knytt/ui/SettingsScreen.tscn");
+        pause();
+    }
+
+    public override void _Process(float delta)
+    {
+        if (Input.IsActionJustPressed("pause")) { unpause(); }
+    }
+
+    public override void _Notification(int what)
+    {
+		if (what == MainLoop.NotificationWmGoBackRequest) { unpause(); }
+    }
+
+    private void pause()
+    {
+        GetTree().Paused = true;
+    }
+
+    private void unpause()
+    {
+        GetTree().Paused = false;
+        GetParent().QueueFree();
+    }
+
+    public void _on_ResumeButton_pressed()
+    {
+        unpause();
+    }
+
+    public void _on_SettingsButton_pressed()
+    {
+        var settings_node = this.settings_scene.Instance() as SettingsScreen;
+        GetParent().AddChild(settings_node);
+    }
+
+    public void _on_QuitButton_pressed()
+    {
+        quit();
+    }
+
+    private async void quit()
+    {
+        var fade = GetNode<FadeLayer>("../../FadeLayer");
+        fade.startFade();
+        await ToSignal(fade, "FadeDone");
+        GetTree().Paused = false;
+        GetTree().ChangeScene("res://knytt/ui/MainMenu.tscn");
+    }
+}
