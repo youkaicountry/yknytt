21c75ac2d859bbe8167f72e2c7e2bed3d6254a93
Mike Rodionov <left.right.forward@gmail.com>
2020-10-19 17:38:23 +0700

    Add elemental objects

--- knytt/objects/GDKnyttObjectFactory.cs
+++ knytt/objects/GDKnyttObjectFactory.cs
@@ -58,6 +58,8 @@ public static class GDKnyttObjectFactory
         ObjectLookup[new KnyttPoint(2, 15)] = "BuzzFlyer";
         ObjectLookup[new KnyttPoint(2, 16)] = "BuzzFlyer";
         ObjectLookup[new KnyttPoint(2, 17)] = "BuzzFlyer";
+        ObjectLookup[new KnyttPoint(2, 18)] = "Elemental";
+        ObjectLookup[new KnyttPoint(2, 19)] = "Elemental";
         ObjectLookup[new KnyttPoint(2, 20)] = "FlySpike";
         ObjectLookup[new KnyttPoint(2, 21)] = "FlySpike";
         ObjectLookup[new KnyttPoint(3, 5)] = "FollowPeaceful";
---
+++ knytt/objects/banks/bank2/Elemental.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+using System.Threading.Tasks;
+
+public class Elemental : GDKnyttBaseObject
+{
+    private AnimatedSprite sprite;
+    
+    public override void _Ready()
+    {
+        sprite = GetNode<AnimatedSprite>("AnimatedSprite");
+        sprite.Play($"{ObjectID.y}_stopped");
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        base._PhysicsProcess(delta);
+        // TODO: don't know what "Flag 0" is, ignoring it
+        if (!Juni.dead && Juni.manhattanDistance(Center) < 67 && !(
+            Juni.ApparentPosition.x - GDArea.GlobalPosition.x < 18 + 16 ||
+            Juni.ApparentPosition.y - GDArea.GlobalPosition.y < 18 + 16 ||
+            GDArea.GlobalPosition.x + GDKnyttArea.Width - Juni.ApparentPosition.x < 18 + 16 ||
+            GDArea.GlobalPosition.y + GDKnyttArea.Height - Juni.ApparentPosition.y < 18 + 16))
+        {
+            sprite.Play($"{ObjectID.y}_launching");
+            GetNode<AudioStreamPlayer2D>($"{ObjectID.y}_ExplodePlayer").Play();
+            Juni.die();
+        }
+    }
+    
+    private void _on_AnimatedSprite_animation_finished()
+    {
+        if (!sprite.Animation.EndsWith("stopped")) { sprite.Play($"{ObjectID.y}_stopped"); }
+    }
+
+    private void _on_Timer_timeout()
+    {
+        if (sprite.Animation.EndsWith("stopped")) { sprite.Play($"{ObjectID.y}_walking"); }
+    }
+}
