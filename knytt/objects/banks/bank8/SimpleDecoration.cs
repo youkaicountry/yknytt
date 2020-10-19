ccc2f4386a9256d16f5e72fc6375e60455279783
Nathaniel Caldwell <nbcwell@gmail.com>
2020-10-19 06:28:31 -0400

    Add decoration objects

---
+++ knytt/objects/banks/bank8/SimpleDecoration.cs
@@ -0,0 +1,10 @@
+using Godot;
+
+public class SimpleDecoration : GDKnyttBaseObject
+{
+    public override void _Ready()
+    {
+        base._Ready();
+        GetNode<AnimatedSprite>("AnimatedSprite").Play(""+ObjectID.y);
+    }
+}
