49216cc6177a65da4a3efc73b23db6124d6ed215
Nathaniel Caldwell <nbcwell@gmail.com>
2020-10-20 00:36:16 -0400

    Add star

---
+++ knytt/objects/banks/bank8/Star.cs
@@ -0,0 +1,43 @@
+using Godot;
+using YUtil.Random;
+
+public class Star : GDKnyttBaseObject
+{
+    const float MIN_SLEEP = 2f;
+    const float MAX_SLEEP = 10f;
+
+    public override void _Ready()
+    {
+        startSleep();
+    }
+
+    private void startSleep()
+    {
+        GetNode<AnimatedSprite>("AnimatedSprite").Visible = false;
+        var timer = GetNode<Timer>("SleepTimer");
+        timer.WaitTime = GDKnyttDataStore.random.NextFloat(MIN_SLEEP, MAX_SLEEP);
+        timer.Start();
+    }
+
+    private void startAnimation()
+    {
+        var anim = GetNode<AnimatedSprite>("AnimatedSprite");
+        anim.Position = new Vector2(GDKnyttDataStore.random.NextFloat(24f), GDKnyttDataStore.random.NextFloat(24f));
+
+        anim.Visible = true;
+        anim.Stop();
+        anim.Frame = 0;
+        anim.Play(""+GDKnyttDataStore.random.Next(0, 4));
+    }
+
+    public void _on_AnimatedSprite_animation_finished()
+    {
+        startSleep();
+    }
+
+    public void _on_SleepTimer_timeout()
+    {
+        startAnimation();
+    }
+
+}
