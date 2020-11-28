47201489124be2805716afe497088dc15dbef032
Mike Rodionov <left.right.forward@gmail.com>
2020-11-28 22:31:30 +0700

    Bullet deceleration support and 13:4 Up Cannon, 5:1 Shadow Witch, 6:6 Trap Fire objects
    Bullet deceleration is not ready yet, because values are interpreted differently for Bouncing Ball and Pinball Movement. Tweaking is required.

--- knytt/objects/banks/bank6/LabyrinthSpike.cs
+++ knytt/objects/banks/bank6/LabyrinthSpike.cs
@@ -1,5 +1,4 @@
 using Godot;
-using System;
 using YUtil.Random;
 
 public class LabyrinthSpike : GDKnyttBaseObject
@@ -14,25 +13,17 @@ public class LabyrinthSpike : GDKnyttBaseObject
         var collision = Call("move_and_collide", delta * speed * direction, true, true, true) as KinematicCollision2D;
         if (collision != null)
         {
-            if (collision.Collider is Juni juni)
+            int retry = 5;
+            while (collision != null)
             {
-                // TODO: Could get stuck in textures if collides both Juni and a wall
-                juni.die();
+                direction = direction == Vector2.Up || direction == Vector2.Down ?
+                    (GDKnyttDataStore.random.NextBoolean() ? Vector2.Left : Vector2.Right) :
+                    (GDKnyttDataStore.random.NextBoolean() ? Vector2.Down : Vector2.Up);
+                collision = Call("move_and_collide", delta * speed * direction, true, true, true) as KinematicCollision2D;
+                if (retry-- <= 0) { return; }
             }
-            else
-            {
-                int retry = 5;
-                while (collision != null)
-                {
-                    direction = direction == Vector2.Up || direction == Vector2.Down ?
-                        (GDKnyttDataStore.random.NextBoolean() ? Vector2.Left : Vector2.Right) :
-                        (GDKnyttDataStore.random.NextBoolean() ? Vector2.Down : Vector2.Up);
-                    collision = Call("move_and_collide", delta * speed * direction, true, true, true) as KinematicCollision2D;
-                    if (retry-- <= 0) { return; }
-                }
-                onCollide();
-                GetNode<AudioStreamPlayer2D>("HitSound2D").Play();
-            }
+            onCollide();
+            GetNode<AudioStreamPlayer2D>("HitSound2D").Play();
         }
         Translate(delta * speed * direction);
     }
