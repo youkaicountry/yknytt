b655ac2bff8dfec49fdaea24e21ab4d617c54451
Mike Rodionov <left.right.forward@gmail.com>
2020-12-16 00:29:34 +0700

    Add decorations from bank 8

--- knytt/objects/banks/bank8/Star.cs
+++ knytt/objects/banks/bank8/Star.cs
@@ -1,43 +1,24 @@
 using Godot;
 using YUtil.Random;
 
-public class Star : GDKnyttBaseObject
+public class Star : GesturesObject
 {
-    const float MIN_SLEEP = 2f;
-    const float MAX_SLEEP = 10f;
-
     public override void _Ready()
     {
-        startSleep();
+        base._Ready();
+        GDArea.Selector.Register(this);
     }
 
-    private void startSleep()
+    protected override void nextAnimation()
     {
-        GetNode<AnimatedSprite>("AnimatedSprite").Visible = false;
-        var timer = GetNode<Timer>("SleepTimer");
-        timer.WaitTime = random.NextFloat(MIN_SLEEP, MAX_SLEEP);
-        timer.Start();
+        if (GDArea.Selector.IsObjectSelected(this))
+        {
+            sprite.Position = new Vector2(random.NextFloat(24f), random.NextFloat(24f));
+            base.nextAnimation();
+        }
+        else
+        {
+            idle();
+        }
     }
-
-    private void startAnimation()
-    {
-        var anim = GetNode<AnimatedSprite>("AnimatedSprite");
-        anim.Position = new Vector2(random.NextFloat(24f), random.NextFloat(24f));
-
-        anim.Visible = true;
-        anim.Stop();
-        anim.Frame = 0;
-        anim.Play(""+random.Next(0, 4));
-    }
-
-    public void _on_AnimatedSprite_animation_finished()
-    {
-        startSleep();
-    }
-
-    public void _on_SleepTimer_timeout()
-    {
-        startAnimation();
-    }
-
 }
