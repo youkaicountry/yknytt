cd624926a3bddc51968dc07f40c98c137ae4ed94
Mike Rodionov <left.right.forward@gmail.com>
2020-12-16 00:57:05 +0700

    Add WalkingShooter and inherited creatures (17:1, 17:7, 4:12, 4:15, 4:16)

--- knytt/objects/banks/bank17/DarkRandomizer.cs
+++ knytt/objects/banks/bank17/DarkRandomizer.cs
@@ -1,15 +1,10 @@
 using Godot;
-using System;
 using YUtil.Random;
 
-public class DarkRandomizer : Muff
+public class DarkRandomizer : WalkingShooter
 {
-    private bool shoot = false;
-
     public override void _Ready()
     {
-        base._Ready();
-        GDArea.Selector.Register(this);
         GDArea.Bullets.RegisterEmitter(this, "DropStuff",
             (p, i) => 
             {
@@ -18,30 +13,6 @@ public class DarkRandomizer : Muff
                 p.DirectionMMF2 = 8 + random.Next(1, 5) * (random.NextBoolean() ? 1 : -1);
                 p.GravityMMF2 = 12;
             });
-    }
-
-    private async void _on_ShotTimer_timeout_ext()
-    {
-        shoot = !shoot;
-        if (shoot)
-        {
-            speed = 0;
-            sprite.Play("ready");
-            await ToSignal(sprite, "animation_finished");
-            if (GDArea.Selector.IsObjectSelected(this))
-            {
-                GetNode<AudioStreamPlayer2D>("ShotPlayer").Play();
-                for (int i = 0; i < 10; i++)
-                {
-                    GDArea.Bullets.Emit(this, i);
-                }
-            }
-            sprite.Play("aftershot");
-        }
-        else
-        {
-            changeSpeed(8 + random.Next(5));
-            _on_DirectionTimer_timeout();
-        }
+        base._Ready();
     }
 }
