12065e4fbfa43502fbc4f7e1dca81a6bcea27ab7
Mike Rodionov <left.right.forward@gmail.com>
2021-01-10 08:11:25 +0700

    Add GhostMod

--- knytt/objects/banks/common/BaseEater.cs
+++ knytt/objects/banks/common/BaseEater.cs
@@ -1,23 +1,12 @@
 using Godot;
-using System;
 
 public class BaseEater : GDKnyttBaseObject
 {
-    public override void _Ready()
-    {
-        OrganicEnemy = true;
-    }
-    
-    public void OnArea2DBodyEntered(GDKnyttBaseObject this_obj, object body)
-    {
-        if (!(body is Juni juni)) { return; }
-        this_obj.GetNode<AnimatedSprite>("AnimatedSprite").Play("eat");
-        this_obj.GetNode<AudioStreamPlayer2D>("Player").Play();
-        juniDie(juni);
-    }
-        
     private void _on_Area2D_body_entered(object body)
     {
-        OnArea2DBodyEntered(this, body);
+        if (!(body is Juni juni)) { return; }
+        GetNode<AnimatedSprite>("AnimatedSprite").Play("eat");
+        GetNode<AudioStreamPlayer2D>("Player").Play();
+        juniDie(juni);
     }
 }
