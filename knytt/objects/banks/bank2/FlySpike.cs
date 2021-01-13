e4baae5da9ed4a7e7eca81466035fa1c60fa4707
Mike Rodionov <left.right.forward@gmail.com>
2021-01-13 10:36:32 +0700

    Split FlySpike in two scenes

--- knytt/objects/banks/bank2/FlySpike.cs
+++ knytt/objects/banks/bank2/FlySpike.cs
@@ -5,49 +5,43 @@ public class FlySpike : GDKnyttBaseObject
 {
     AnimatedSprite animated;
     Timer wait_timer;
-    CosineMod mod;
-    public bool FacingRight { get { return !animated.FlipH; } set { animated.FlipH = !value; } }
+    [Export] public bool FacingRight = true;
 
-    private const float MIN_WAIT = 1f;
-    private const float MAX_WAIT = 4f;
-    private const float SPEED = 192f;
+    private const float MIN_WAIT = 1.6f;
+    private const float MAX_WAIT = 3.2f;
+    private float speed = 200;
 
     public override void _Ready()
     {
         animated = GetNode<AnimatedSprite>("AnimatedSprite");
         wait_timer = GetNode<Timer>("WaitTimer");
-        mod = GetNode<CosineMod>("CosineMod");
-        if (ObjectID.y == 21) 
-        { 
-            animated.Animation = "1";
-            FacingRight = false;
-        }
+        animated.FlipH = !FacingRight;
         start();
     }
 
     private async void stop()
     {
-        mod.stop();
+        FacingRight = !FacingRight;
         animated.Stop();
         animated.Frame = 0;
-        FacingRight = !FacingRight;
+        animated.FlipH = !FacingRight;
         wait_timer.WaitTime = random.NextFloat(MIN_WAIT, MAX_WAIT);
         wait_timer.Start();
         await ToSignal(wait_timer, "timeout");
+        speed = (3 + random.Next(2)) * 50;
         start();
     }
 
     private void start()
     {
         animated.Play();
-        mod.start();
     }
 
     public override void _PhysicsProcess(float delta)
     { 
         base._PhysicsProcess(delta);
         if (wait_timer.TimeLeft > 0f) { return; }
-        Translate(new Vector2(SPEED * delta * (FacingRight ? 1f : -1f), 0f));
+        Translate(new Vector2(speed * delta * (FacingRight ? 1f : -1f), 0f));
     }
 
     public void _on_WallHitArea_body_entered(Node body)
