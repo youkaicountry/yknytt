769dd4132237a3ec7667f10dd6c8350241699eb2
Nathaniel Caldwell <nbcwell@gmail.com>
2021-01-23 17:56:18 -0500

    Improve NoJump check
    The old implementation was weird, now Juni tracks the number of NoJump zones she is in

--- knytt/objects/banks/bank0/NoJump.cs
+++ knytt/objects/banks/bank0/NoJump.cs
@@ -1,1 +1,16 @@
-public class NoJump : GDKnyttBaseObject { }
+using Godot;
+
+public class NoJump : GDKnyttBaseObject 
+{ 
+    public void _on_Area2D_body_entered(Node body)
+    {
+        if (!(body is Juni juni)) { return; }
+        juni.CanJump = false;
+    }
+
+    public void _on_Area2D_body_exited(Node body)
+    {
+        if (!(body is Juni juni)) { return; }
+        juni.CanJump = true;
+    }
+}
