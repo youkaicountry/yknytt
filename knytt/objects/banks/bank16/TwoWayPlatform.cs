2855eb8f95c3ebe6d280b74fa0bd4326d98df325
up-left <left.right.forward@gmail.com>
2021-01-24 20:59:22 +0700

    Fix TwoWayPlatform + remove wrong frame from PowerItem

--- knytt/objects/banks/bank16/TwoWayPlatform.cs
+++ knytt/objects/banks/bank16/TwoWayPlatform.cs
@@ -5,29 +5,28 @@ using System.Linq;
 public class TwoWayPlatform : OneWayPlatform
 {
     private HashSet<Juni> junis = new HashSet<Juni>();
-    private CollisionShape2D noJumpShape;
-
-    public override void _Ready()
-    {
-        noJumpShape = GetNode<CollisionShape2D>("NoJumpArea/CollisionShape2D");
-    }
 
     public override void _PhysicsProcess(float delta)
     {
-        bool enable_nojump = junis.Any(j => j.juniInput.DownHeld);
-        if (!enable_nojump != noJumpShape.Disabled) { noJumpShape.SetDeferred("disabled", !enable_nojump); }
+        foreach (Juni juni in junis)
+        {
+            if (juni.juniInput.checkJustPressed("down")) { juni.CanJump = false; }
+            if (juni.juniInput.checkJustReleased("down") && !juni.CanJump) { juni.CanJump = true; }
+        }
 
-        bool disable_ground = enable_nojump && junis.Any(j => j.juniInput.DownHeld && j.juniInput.JumpEdge);
+        bool disable_ground = junis.Any(j => j.juniInput.DownHeld && j.juniInput.JumpEdge);
         if (disable_ground) { disablePlatform(true); }
     }
 
     private void _on_EnterArea_body_entered(Juni juni)
     {
+        if (juni.juniInput.DownHeld) { juni.CanJump = false; }
         junis.Add(juni);
     }
 
     private void _on_EnterArea_body_exited(Juni juni)
     {
+        if (juni.juniInput.DownHeld) { juni.CanJump = true; }
         junis.Remove(juni);
     }
 }
