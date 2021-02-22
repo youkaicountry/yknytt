2039df2ae95557bf31ba30d8ed6ce9abe803b991
Nathaniel Caldwell <nbcwell@gmail.com>
2021-02-21 23:33:31 -0500

    Add noclip fly debug mode
    This is very helpful for debugging.
    Currently it's mapped to the ctrl key for easy access.

--- knytt/juni/GroundChecker.cs
+++ knytt/juni/GroundChecker.cs
@@ -2,5 +2,11 @@ using Godot;
 
 public class GroundChecker : Area2D
 {
+    public bool Disabled 
+    { 
+        get { return GetNode<CollisionPolygon2D>("CollisionPolygon2D").Disabled; }
+        set { GetNode<CollisionPolygon2D>("CollisionPolygon2D").Disabled = value; }
+    }
+
     public bool IsOnGround { get { return GetOverlappingBodies().Count > 0; } }
 }
