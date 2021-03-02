200f98b5549d9ab68268483df787fc8d22d15325
Mike Rodionov <left.right.forward@gmail.com>
2021-03-02 11:25:42 +0700

    Small fixes [2]
    Do not freeze on invalid cutscene
    Handle wrong shift type
    Fix and tweak HomingEnemy

--- knytt/GDKnyttDataStore.cs
+++ knytt/GDKnyttDataStore.cs
@@ -46,7 +46,7 @@ public class GDKnyttDataStore : Node
         CutsceneAfter = null;
         CutsceneReturn = Tree.CurrentScene;
         CutsceneSound = sound;
-        if (!KWorld.worldFileExists(Cutscene.makeScenePath(1))) { return; }
+        if (!KWorld.worldFileExists(Cutscene.makeScenePath(1))) { Tree.Paused = false; return; }
         Tree.Paused = true;
         Tree.Root.RemoveChild(Tree.CurrentScene);
         // TODO: how not to blink?
--- knytt/objects/banks/bank2/HomingEnemy.cs
+++ knytt/objects/banks/bank2/HomingEnemy.cs
@@ -5,10 +5,9 @@ public class HomingEnemy : GDKnyttBaseObject
     [Export] protected int initialSpeed = 0;
     [Export] protected int wallSpeed = 0;
     [Export] protected int airSpeed = 0;
-    [Export] protected float slowingDown = 1;
     [Export] protected Color wallColor = new Color(1, 1, 1, 0.5f);
 
-    protected const float SPEED_SCALE = 50f / 8;
+    protected const float SPEED_SCALE = 48f / 8;
     protected const float JUNI_MIN_DISTANCE = 6;
 
     protected int speed;
@@ -30,15 +29,11 @@ public class HomingEnemy : GDKnyttBaseObject
         float distance = Juni.distance(Center);
         if (distance > JUNI_MIN_DISTANCE)
         {
-            var inertia = 0.85f - delta * 3; // TODO: replace this magic formula with more correct and fps-independent behaviour
+            var inertia = 0.9f - delta * 3; // TODO: replace this magic formula with more correct and fps-independent behaviour
             var new_speed_vector = speed * SPEED_SCALE * (Juni.ApparentPosition - Center) / distance;
             speedVector = speedVector * inertia + new_speed_vector * (1 - inertia);
-            Translate(speedVector * delta);
         }
-        else
-        {
-            Translate(speedVector * delta * slowingDown);
-        }
+        Translate(speedVector * delta);
     }
 
     private void _on_Area2D_body_entered(object body)
@@ -51,7 +46,7 @@ public class HomingEnemy : GDKnyttBaseObject
 
     private void _on_Area2D_body_exited(object body)
     {
-        if (GetNode<Area2D>("Area2D").GetOverlappingBodies().Count > 0) { return; }
+        if (GetNode<Area2D>("Area2D").GetOverlappingBodies().Count > 1) { return; }
         Modulate = new Color(1, 1, 1, 1);
         speed = airSpeed;
         if (hasWallAnimation) { sprite.Play("default"); }
