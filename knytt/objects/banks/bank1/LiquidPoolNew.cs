38df305b6b5a86aed537fe86a849c8a83b12b836
Mike Rodionov <left.right.forward@gmail.com>
2021-03-07 07:32:08 +0700

    Diffferent collision shapes for different pools

---
+++ knytt/objects/banks/bank1/LiquidPoolNew.cs
@@ -0,0 +1,26 @@
+using Godot;
+using YUtil.Random;
+
+public class LiquidPoolNew : GDKnyttBaseObject
+{
+    protected AnimatedSprite player;
+    bool ping = true;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        GetNodeOrNull<AnimatedSprite>("Halo")?.Play();
+        
+        player = GetNode<AnimatedSprite>("AnimatedSprite");
+        player.SpeedScale = random.NextFloat(.6f, 1f);
+        player.Play();
+        player.Frame = random.Next(player.Frames.GetFrameCount(player.Animation));
+    }
+
+    public void _on_AnimatedSprite_animation_finished()
+    {
+        player.Stop();
+        player.Play(backwards: ping);
+        ping = !ping;
+    }
+}
