7b6d6bbb581018cfeee3ef1e67d33d9f5bbec4f3
Mike Rodionov <left.right.forward@gmail.com>
2021-03-12 12:31:19 +0700

    Flythings from bank 2
    + DisappearWhenStopped by default for bullets
    + fixed EnableRotation for bullets
    + complete bank 17
    + rewrite BuzzFlyer

---
+++ knytt/objects/banks/bank2/HomingFlything.cs
@@ -0,0 +1,27 @@
+using Godot;
+
+public class HomingFlything : BuzzFlyer
+{
+    public override void _Ready()
+    {
+        base._Ready();
+        GDArea.Bullets.RegisterEmitter(this, "HomingBullet",
+            (p, i) =>
+            {
+                (p as HomingBullet).globalJuni = Juni;
+                p.Translate(new Vector2(12, 12));
+                p.Velocity = 100 * Mathf.Sqrt2;
+                p.Direction = Mathf.Pi / 4 + i * Mathf.Pi / 2;
+            });
+    }
+
+    private void _on_ShootTimer_timeout_ext()
+    {
+        currentDirection = Vector2.Zero;
+        GetNode<RawAudioPlayer2D>("ShootPlayer").Play();
+        for (int i = 0; i < 4; i++)
+        {
+            GDArea.Bullets.Emit(this, i);
+        }
+    }
+}
