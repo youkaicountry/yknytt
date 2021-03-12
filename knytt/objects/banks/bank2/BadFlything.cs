39c240e00f793063e97094c01a164bb39cf2731b
Mike Rodionov <left.right.forward@gmail.com>
2021-03-12 12:31:19 +0700

    Flythings from bank 2
    + DisappearWhenStopped by default for bullets
    + fixed EnableRotation for bullets
    + complete bank 17
    + rewrite BuzzFlyer

---
+++ knytt/objects/banks/bank2/BadFlything.cs
@@ -0,0 +1,29 @@
+using Godot;
+
+public class BadFlything : BuzzFlyer
+{
+    [Export] public int[] directions = null;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        GDArea.Bullets.RegisterEmitter(this, "FlythingBullet",
+            (p, i) =>
+            {
+                p.Translate(new Vector2(12, 12));
+                p.VelocityMMF2 = 30;
+                p.DirectionMMF2 = i;
+                p.EnableRotation = true;
+            });
+    }
+
+    private void _on_ShootTimer_timeout_ext()
+    {
+        currentDirection = Vector2.Zero;
+        GetNode<RawAudioPlayer2D>("ShootPlayer").Play();
+        foreach (int dir in directions)
+        {
+            GDArea.Bullets.Emit(this, dir);
+        }
+    }
+}
---
+++ knytt/objects/banks/bank2/DropFlything.cs
@@ -0,0 +1,55 @@
+using Godot;
+using System.Linq;
+
+public class DropFlything : BuzzFlyer
+{
+    [Export] protected int[] directions = null;
+    [Export] protected int bulletsCount = 0;
+
+    private static readonly Vector2[] directionsOverride = Directions.Where(d => d != Vector2.Zero).ToArray();
+
+    protected Timer bulletTimer;
+    protected int counter;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        bulletTimer = GetNode<Timer>("BulletTimer");
+        GDArea.Selector.Register(this);
+        GDArea.Bullets.RegisterEmitter(this, "DropStuff",
+            (p, i) =>
+            {
+                p.Translate(new Vector2(12 + (i > 8 && i < 24 ? -6 : 6), 12 + (i < 16 ? -4 : 4)));
+                p.DirectionMMF2 = i;
+                p.VelocityMMF2 = 30 + random.Next(2);
+                p.GravityMMF2 = 12;
+            });
+    }
+
+    private void _on_ShootTimer_timeout_ext()
+    {
+        counter = 0;
+        bulletTimer.Start();
+        GetNode<RawAudioPlayer2D>("ShootPlayer").Play();
+    }
+
+    private void _on_BulletTimer_timeout()
+    {
+        foreach (int dir in directions)
+        {
+            if (GDArea.Selector.IsObjectSelected(this))
+            {
+                GDArea.Bullets.Emit(this, dir);
+            }
+        }
+        if (++counter >= bulletsCount)
+        {
+            bulletTimer.Stop();
+        }
+    }
+
+    protected override void changeDirection()
+    {
+        currentDirection = random.NextElement(directionsOverride);
+    }
+}
