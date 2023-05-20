fd16d238b925a867600e5cfc153444e313d23721
up-left <left.right.forward@gmail.com>
2023-05-20 21:03:46 +0700

    Console commands for save files and Juni's powers
    First one should help to extract saves from closed filesystem
    Second one is useful for quick testing

--- knytt/ui/console/ConsoleCommands.cs
+++ knytt/ui/console/ConsoleCommands.cs
@@ -1,14 +1,27 @@
 using Godot;
 using YKnyttLib.Parser;
+using YKnyttLib;
+using System;
 
 public static class ConsoleCommands
 {
     public static CommandSet BuildCommandSet()
     {
         var cs = new CommandSet();
-        cs.AddCommand(new CommandDeclaration("speed", "View or set the speed of the game", false, SpeedCommand.NewSpeedCommand, new CommandArg("value", CommandArg.Type.FloatArg, optional: true)));
-        cs.AddCommand(new CommandDeclaration("help", "View help for a given command", false, HelpCommand.NewHelpCommand, new CommandArg("name", CommandArg.Type.StringArg, optional: true)));
-        cs.AddCommand(new CommandDeclaration("list", "View the list of commands", false, ListCommand.NewListCommand));
+        cs.AddCommand(new CommandDeclaration("speed", "View or set the speed of the game", null, false, SpeedCommand.NewSpeedCommand, new CommandArg("value", CommandArg.Type.FloatArg, optional: true)));
+        cs.AddCommand(new CommandDeclaration("help", "View help for a given command", null, false, HelpCommand.NewHelpCommand, new CommandArg("name", CommandArg.Type.StringArg, optional: true)));
+        cs.AddCommand(new CommandDeclaration("list", "View the list of commands", null, false, ListCommand.NewListCommand));
+        cs.AddCommand(new CommandDeclaration("save", "Saves current game. Also copies and pastes save file from clipboard.", 
+            "save: saves game at current position\n" +
+            "save print: prints save file\n" +
+            "save copy: copies save to clipboard\n" +
+            "save paste: pastes save from clipboard", 
+            false, SaveCommand.NewSaveCommand, new CommandArg("subcmd", CommandArg.Type.StringArg, optional: true)));
+        cs.AddCommand(new CommandDeclaration("set", "Sets Juni's power or flag",
+            "powers: run climb doublejump highjump eye enemydetector umbrella hologram " + 
+            "redkey yellowkey bluekey purplekey map flag0 .. flag9\nvalue: on off 1 0",
+            false, SetCommand.NewSetCommand, new CommandArg("variable", CommandArg.Type.StringArg, optional: false), 
+            new CommandArg("value", CommandArg.Type.StringArg, optional: false)));
         return cs;
     }
 
@@ -78,7 +91,7 @@ public static class ConsoleCommands
             }
 
             var decl = env.Parser.Commands.Name2Command[name];
-            env.Console.AddMessage($"{decl.Description}");
+            env.Console.AddMessage($"{decl.DetailedDescription ?? decl.Description}");
             env.Console.AddMessage($"Usage: {decl}");
 
             return null;
@@ -103,6 +116,101 @@ public static class ConsoleCommands
             var list = env.Parser.Commands.MakeList(true);
             env.Console.AddMessage(string.Join("\n", list));
 
+            return null;
+        }
+    }
+
+    public class SaveCommand : ICommand
+    {
+        string subcmd;
+
+        public SaveCommand(CommandParseResult result)
+        {
+            subcmd = result.Args["subcmd"];
+        }
+
+        public static ICommand NewSaveCommand(CommandParseResult result)
+        {
+            return new SaveCommand(result);
+        }
+
+        public string Execute(object environment)
+        {
+            var env = (ConsoleExecutionEnvironment)environment;
+            GDKnyttGame game = GDKnyttDataStore.Tree.Root.FindNode("GKnyttGame", owned: false) as GDKnyttGame;
+            switch (subcmd)
+            {
+                case null:
+                    game.saveGame(game.Juni, write: true);
+                    break;
+                case "print":
+                    env.Console.AddMessage(game.GDWorld.KWorld.CurrentSave.ToString());
+                    break;
+                case "copy":
+                    OS.Clipboard = game.GDWorld.KWorld.CurrentSave.ToString();
+                    env.Console.AddMessage("Save file was copied to clipboard.");
+                    break;
+                case "paste":
+                    KnyttSave save;
+                    try
+                    {
+                        save = new KnyttSave(game.GDWorld.KWorld, OS.Clipboard, game.GDWorld.KWorld.CurrentSave.Slot);
+                    }
+                    catch (Exception)
+                    {
+                        return "Can't parse save file from clipboard";
+                    }
+                    game.GDWorld.KWorld.CurrentSave = save;
+                    game.saveGame(save);
+                    game.Juni.die();
+                    break;
+            }
+            return null;
+        }
+    }
+
+    public class SetCommand : ICommand
+    {
+        private static string[] names = Enum.GetNames(typeof(JuniValues.PowerNames));
+        string variable;
+        string value;
+
+        public SetCommand(CommandParseResult result)
+        {
+            variable = result.Args["variable"];
+            value = result.Args["value"];
+        }
+
+        public static ICommand NewSetCommand(CommandParseResult result)
+        {
+            return new SetCommand(result);
+        }
+
+        public string Execute(object environment)
+        {
+            var env = (ConsoleExecutionEnvironment)environment;
+            GDKnyttGame game = GDKnyttDataStore.Tree.Root.FindNode("GKnyttGame", owned: false) as GDKnyttGame;
+
+            bool? bvalue = null;
+            if (value == "on" || value == "1") { bvalue = true; }
+            if (value == "off" || value == "0") { bvalue = false; }
+
+            int power_index = Array.FindIndex(names, x => x.ToLower() == variable);
+            if (power_index != -1 && bvalue != null)
+            {
+                game.Juni.setPower(power_index, bvalue ?? true);
+                env.Console.AddMessage($"{((JuniValues.PowerNames)power_index).ToString()} set to {value}");
+            }
+            else if (variable.StartsWith("flag") && bvalue != null && 
+                     int.TryParse(variable.Substring(4), out var flag_index) && flag_index >= 0 && flag_index < 10)
+            {
+                game.Juni.Powers.setFlag(flag_index, bvalue ?? true);
+                env.Console.AddMessage($"Flag {flag_index} set to {value}");
+            }
+            else
+            {
+                return "Can't recognize variable or value";
+            }
             return null;
         }
     }
