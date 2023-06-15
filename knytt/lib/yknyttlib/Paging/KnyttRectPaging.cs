e3d8960ca8c5344d8a24145dfbcaa02f05ed6256
up-left <left.right.forward@gmail.com>
2023-06-15 22:04:10 +0700

    Revert background area loading
    Can't run AddChild in a separate Task due to Godot errors
    CallDefered is not a solution, it blocks main thread
    Possible options are deferred calls to instantiateArea by timer, or when Juni in IdleState (hard)

--- knytt/lib/yknyttlib/Paging/KnyttRectPaging.cs
+++ knytt/lib/yknyttlib/Paging/KnyttRectPaging.cs
@@ -1,14 +1,9 @@
 ﻿using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace YKnyttLib.Paging
 {
     public class KnyttRectPaging<T> : KnyttPagingPolicy<T>
     {
-        private LinkedList<KnyttPoint> q_remove = new LinkedList<KnyttPoint>();
-        private LinkedList<KnyttPoint> q_add = new LinkedList<KnyttPoint>();
-        private Task task = null;
-
         public KnyttPoint BorderSize { get; private set; }
 
         public KnyttRectPaging(KnyttPoint border_size)
@@ -16,48 +11,37 @@ namespace YKnyttLib.Paging
             this.BorderSize = border_size;
         }
 
+        // This is O(n) time
         protected override void _handlePaging(KnyttPoint location)
         {
-            // Iterate over the current set + queue, and remove 
+            List<KnyttPoint> q_remove = new List<KnyttPoint>();
+            List<KnyttPoint> q_add = new List<KnyttPoint>();
+            // Iterate over the current set, and remove 
             foreach (var l in this.Areas.Keys)
             {
-                if (!isIn(l, location)) { q_remove.AddLast(l); }
+                if (!isIn(l, location)) { q_remove.Add(l); }
             }
 
-            for (var node = q_add.First; node != null; )
-            {
-                var next = node.Next;
-                if (!isIn(node.Value, location)) { q_add.Remove(node); }
-                node = next;
-            }
-
-            // Iterate over the new area and add any that aren't in this.Areas or queue
+            // Iterate over the new area and add any that aren't in this.Areas
             for (int y = location.y - BorderSize.y; y <= location.y + BorderSize.y; y++)
             {
                 for (int x = location.x - BorderSize.x; x <= location.x + BorderSize.x; x++)
                 {
                     var kp = new KnyttPoint(x, y);
-                    if (!Areas.ContainsKey(kp) && !q_add.Contains(kp)) { q_add.AddLast(kp); }
-                    q_remove.Remove(kp);
+                    if (!Areas.ContainsKey(kp)) { q_add.Add(kp); }
                 }
             }
 
-            if (q_add.Contains(location)) // central area cannot wait
+            // Now just parse the queues
+            foreach (var l in q_remove)
             {
-                q_add.Remove(location);
-                pageIn(location);
+                pageOut(l);
             }
 
-            if (task != null) { return; }
-            task = Task.Run(() => {
-                // Now just parse the queues
-                while (q_remove.Count > 0 && q_add.Count > 0)
-                {
-                    while (q_remove.Count > 0) { pageOut(q_remove.PopFirst()); }
-                    while (q_add.Count > 0) { pageIn(q_add.PopFirst()); }
-                }
-                task = null;
-            });
+            foreach (var l in q_add)
+            {
+                pageIn(l);
+            }
         }
 
         private bool isIn(KnyttPoint location, KnyttPoint test_location)
@@ -65,15 +49,5 @@ namespace YKnyttLib.Paging
             return location.x >= (test_location.x - BorderSize.x) && location.x <= (test_location.x + BorderSize.x) &&
                    location.y >= (test_location.y - BorderSize.y) && location.y <= (test_location.y + BorderSize.y);
         }
-    }
-}
-
-public static class LinkedListExtension
-{
-    public static T PopFirst<T>(this LinkedList<T> list)
-    {
-        var el = list.First.Value;
-        list.RemoveFirst();
-        return el;
     }
 }
