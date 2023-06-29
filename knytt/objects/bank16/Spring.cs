8ce8778e6b197eacae8b147dd2f95c1209a2956a
up-left <left.right.forward@gmail.com>
2023-06-29 22:45:08 +0700

    Bigfixes in 16:1 Spring
    Don't stop playing sound when leaving an area
    Adjust swim jump power
    Don't land on the floor if floor and spring are on the same tile
    Activate when entering from below
    Removed multiplayer code (restore it later if needed)
    Added trace images
    highjump+hold and lowjump+nohold is not tuned. Requires to re-tune all other Juni's constants, which is a lot of work

--- knytt/objects/bank16/Spring.cs
+++ knytt/objects/bank16/Spring.cs
@@ -1,52 +1,41 @@
 using Godot;
-using System.Collections.Generic;
 
 public class Spring : GDKnyttBaseObject
 {
-    HashSet<Juni> junis;
+    Juni juni;
 
     public override void _Ready()
     {
-        junis = new HashSet<Juni>();
-        var sprite = GetNode<Sprite>("Sprite");
-        var m = sprite.Modulate;
-        m.a = 0f;
-        sprite.Modulate = m;
+        GetNode<Sprite>("Sprite").Modulate *= new Color(1, 1, 1, 0);
     }
 
     public override void _PhysicsProcess(float delta)
     {
-        foreach (Juni juni in junis)
-        {
-            // Enforce one-way
-            var jgp = juni.GlobalPosition;
-            var gp = GlobalPosition;
-            if (juni.velocity.y <= 0 || jgp.y > gp.y + 10f) { return; }
+        if (juni == null) { return; }
+        if (juni.GlobalPosition.y > GlobalPosition.y + 10f) { return; }
 
-            // Restore Juni's position to the top of the spring for stability
-            var jbottom = juni.Bottom;
-            jgp.y -= jbottom.y - gp.y;
-            juni.GlobalPosition = jgp;
+        // Restore Juni's position to the top of the spring for stability
+        juni.GlobalPosition -= new Vector2(0, juni.Bottom.y - GlobalPosition.y);
+        juni.MoveAndSlide(Godot.Vector2.Zero, Godot.Vector2.Up, true); // workaround to prevent landing
 
-            // Spring
-            juni.executeJump(-340f, sound: false, reset_jumps: true);
-            GetNode<AudioStreamPlayer2D>("BouncePlayer2D").Play();
+        // Spring
+        // TODO: not accurate for highjump+hold and lowjump+nohold. maybe try to get rid of HIGH_JUMP_DEFAULT_POWER?
+        juni.executeJump(juni.Swim ? -101f : -340f, sound: false, reset_jumps: true);
+        juni.playSound("bounce");
 
-            var anim = GetNode<AnimationPlayer>("AnimationPlayer");
-            anim.Stop();
-            anim.Play("Spring");
-        }
+        var anim = GetNode<AnimationPlayer>("AnimationPlayer");
+        anim.Stop();
+        anim.Play("Spring");
+        juni = null;
     }
 
     public void _on_Area2D_body_entered(Node body)
     {
-        if (!(body is Juni)) { return; }
-        junis.Add(body as Juni);
+        if (body is Juni juni) { this.juni = juni; }
     }
 
     public void _on_Area2D_body_exited(Node body)
     {
-        if (!(body is Juni)) { return; }
-        junis.Remove(body as Juni);
+        if (body is Juni juni && juni == this.juni) { this.juni = null; }
     }
 }
