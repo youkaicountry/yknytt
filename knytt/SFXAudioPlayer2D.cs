4a1bd67365ca0ebc2b874fb4578bec6ac4109c95
up-left <left.right.forward@gmail.com>
2023-07-21 01:00:49 +0700

    Little changes
    Don't log saving a game (may be time-consuming)
    Polish PanningStrength
    Minimize save file
    Itch.io link in credits

--- knytt/SFXAudioPlayer2D.cs
+++ knytt/SFXAudioPlayer2D.cs
@@ -4,6 +4,11 @@ public class SFXAudioPlayer2D : AudioStreamPlayer2D
 {
     public static float GlobalPanningStrength;
 
+    public override void _Ready()
+    {
+        PanningStrength = GlobalPanningStrength;
+    }
+
     public override void _PhysicsProcess(float delta)
     {
         if (PanningStrength != GlobalPanningStrength) { PanningStrength = GlobalPanningStrength; }
