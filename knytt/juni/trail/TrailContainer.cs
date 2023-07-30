6f966b58983df8018c56ef4bff34f1eeb5029b78
youkaicountry <nbcwell@gmail.com>
2023-07-30 17:44:17 -0400

    Add trail command
    This command toggles showing simple debug trails behind Juni. Also
    optionally accepts the approximate number of points to display, and the
    how many frames should pass between updates.

--- knytt/juni/trail/TrailContainer.cs
+++ knytt/juni/trail/TrailContainer.cs
@@ -5,9 +5,29 @@ public class TrailContainer : Node2D
     [Export] public int TrailCount {get; set;} = 100;
     [Export] public int TrailFrames {get; set;} = 5;
 
+    private bool _on = false;
+    public bool On {
+        get { return _on; }
+        set
+        {
+            if (_on)
+            {
+                if (value) { return; }
+            }
+            else
+            {
+                if (value) { reset(); }
+            }
+
+            _on = value;
+            Visible = value;
+        }
+    }
+
     PackedScene trail_scene;
 
     private int step;
+    private int replace;
 
     public GDKnyttGame Game { get; private set; }
 
@@ -20,6 +40,7 @@ public class TrailContainer : Node2D
     public void reset()
     {
         step = 0;
+        replace = 0;
         for (int i = 0; i < GetChildCount(); i++)
         {
            var child = GetChild(i);
@@ -30,15 +51,28 @@ public class TrailContainer : Node2D
     public override void _Ready()
     {
         trail_scene = ResourceLoader.Load("res://knytt/juni/trail/JuniTrail.tscn") as PackedScene;
+        Visible = false;
     }
 
     public override void _PhysicsProcess(float delta)
     {
         if (step++ % TrailFrames == 0)
         {
-            var s = trail_scene.Instance<Sprite>();
+            Sprite s;
+            var children = GetChildCount();
+            if (children >= TrailCount)
+            {
+                if (replace >= children) { replace = 0; }
+                s = GetChild<Sprite>(replace++);
+            }
+            else
+            {
+                s = trail_scene.Instance<Sprite>();
+                AddChild(s);
+            }
+            
             positionTrail(s, Game.Juni);
-            AddChild(s);
+            
         }
     }
 
