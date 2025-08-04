479d06d20c60460fd60fed46918dce3f39378519
youkaicountry <nbcwell@gmail.com>
2025-08-04 15:06:01 -0400

    Use a central variable instead of user://

--- knytt/GDKnyttDataStore.cs
+++ knytt/GDKnyttDataStore.cs
@@ -5,94 +5,96 @@ using System.Linq;
 
 public class GDKnyttDataStore : Node
 {
-    public static Random random = new Random();
-    public static GDKnyttWorldImpl KWorld { get; set; }
+	public static Random random = new Random();
+	public static GDKnyttWorldImpl KWorld { get; set; }
 
-    public static SceneTree Tree { get; set; }
+	public static SceneTree Tree { get; set; }
 
-    public static string ProgressHint { get; set; }
+	public static string ProgressHint { get; set; }
 
-    public static string CutsceneName { get; private set; }
-    public static string CutsceneAfter { get; private set; }
-    public static Node CutsceneReturn { get; private set; }
+	public static string BaseDataDirectory { get; private set; } = OS.GetUserDataDir();
 
-    public enum CutsceneMode { Intro, Middle, Ending };
-    public static CutsceneMode Mode { get; private set; }
-    public static bool CutsceneFadeIn { get; private set; }
-    public static string CutsceneSound { get; private set; }
+	public static string CutsceneName { get; private set; }
+	public static string CutsceneAfter { get; private set; }
+	public static Node CutsceneReturn { get; private set; }
 
-    const int BaseIterations = 60;
-    public static float CurrentSpeed
-    {
-        get { return Godot.Engine.TimeScale; }
-        set
-        {
-            int iterations = (int)(BaseIterations * value);
-            Godot.Engine.TimeScale = value;
-            Godot.Engine.IterationsPerSecond = iterations;
-        }
-    }
+	public enum CutsceneMode { Intro, Middle, Ending };
+	public static CutsceneMode Mode { get; private set; }
+	public static bool CutsceneFadeIn { get; private set; }
+	public static string CutsceneSound { get; private set; }
 
-    public override void _Ready()
-    {
-        Tree = GetTree();
-    }
+	const int BaseIterations = 60;
+	public static float CurrentSpeed
+	{
+		get { return Godot.Engine.TimeScale; }
+		set
+		{
+			int iterations = (int)(BaseIterations * value);
+			Godot.Engine.TimeScale = value;
+			Godot.Engine.IterationsPerSecond = iterations;
+		}
+	}
 
-    public static void startGame(bool new_game)
-    {
-        Tree.Paused = false;
-        if (new_game)
-        {
-            Mode = CutsceneMode.Intro;
-            CutsceneFadeIn = true;
-            startCutscene("Intro", "res://knytt/GDKnyttGame.tscn");
-        }
-        else { Tree.ChangeScene("res://knytt/GDKnyttGame.tscn"); }
-    }
+	public override void _Ready()
+	{
+		Tree = GetTree();
+	}
 
-    public static void winGame(string ending = "Ending")
-    {
-        Mode = CutsceneMode.Ending;
-        CurrentSpeed = 1;
-        CutsceneFadeIn = false;
-        startCutscene(ending, "res://knytt/ui/MainMenu.tscn");
-    }
+	public static void startGame(bool new_game)
+	{
+		Tree.Paused = false;
+		if (new_game)
+		{
+			Mode = CutsceneMode.Intro;
+			CutsceneFadeIn = true;
+			startCutscene("Intro", "res://knytt/GDKnyttGame.tscn");
+		}
+		else { Tree.ChangeScene("res://knytt/GDKnyttGame.tscn"); }
+	}
 
-    private static void startCutscene(string cutscene, string after)
-    {
-        CutsceneName = cutscene;
-        CutsceneAfter = after;
-        CutsceneReturn = null;
-        CutsceneSound = null;
-        if (!KWorld.worldFileExists(Cutscene.makeScenePath(1))) { Tree.ChangeScene(after); return; }
-        GDKnyttSettings.setupViewport(for_ui: true);
-        Tree.ChangeScene("res://knytt/ui/Cutscene.tscn");
-    }
+	public static void winGame(string ending = "Ending")
+	{
+		Mode = CutsceneMode.Ending;
+		CurrentSpeed = 1;
+		CutsceneFadeIn = false;
+		startCutscene(ending, "res://knytt/ui/MainMenu.tscn");
+	}
 
-    public static void playCutscene(string cutscene, string sound)
-    {
-        Mode = CutsceneMode.Middle;
-        CutsceneFadeIn = false;
-        CutsceneName = cutscene;
-        CutsceneAfter = null;
-        CutsceneReturn = Tree.CurrentScene;
-        CutsceneSound = sound;
-        if (!KWorld.worldFileExists(Cutscene.makeScenePath(1)))
-        {
-            if (Tree.Paused) { Tree.Paused = false; Cutscene.releaseAll(); }
-            return;
-        }
-        GDKnyttSettings.setupViewport(for_ui: true);
-        Tree.Paused = true;
-        Tree.Root.RemoveChild(Tree.CurrentScene);
-        Tree.ChangeScene("res://knytt/ui/Cutscene.tscn");
-    }
+	private static void startCutscene(string cutscene, string after)
+	{
+		CutsceneName = cutscene;
+		CutsceneAfter = after;
+		CutsceneReturn = null;
+		CutsceneSound = null;
+		if (!KWorld.worldFileExists(Cutscene.makeScenePath(1))) { Tree.ChangeScene(after); return; }
+		GDKnyttSettings.setupViewport(for_ui: true);
+		Tree.ChangeScene("res://knytt/ui/Cutscene.tscn");
+	}
+
+	public static void playCutscene(string cutscene, string sound)
+	{
+		Mode = CutsceneMode.Middle;
+		CutsceneFadeIn = false;
+		CutsceneName = cutscene;
+		CutsceneAfter = null;
+		CutsceneReturn = Tree.CurrentScene;
+		CutsceneSound = sound;
+		if (!KWorld.worldFileExists(Cutscene.makeScenePath(1)))
+		{
+			if (Tree.Paused) { Tree.Paused = false; Cutscene.releaseAll(); }
+			return;
+		}
+		GDKnyttSettings.setupViewport(for_ui: true);
+		Tree.Paused = true;
+		Tree.Root.RemoveChild(Tree.CurrentScene);
+		Tree.ChangeScene("res://knytt/ui/Cutscene.tscn");
+	}
 }
 
 public static class RandomExtension
 {
-    public static T NextElement<T>(this Random random, ICollection<T> e)
-    {
-        return e.ElementAt(random.Next(e.Count()));
-    }
+	public static T NextElement<T>(this Random random, ICollection<T> e)
+	{
+		return e.ElementAt(random.Next(e.Count()));
+	}
 }
